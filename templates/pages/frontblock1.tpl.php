ed6b98fbb094449fb1d805aee380416d480f1ba0
gbelot2003 <gerardobelot@outlook.com>
2014-07-31 01:35:02 +0000

    General Changes on block 2

--- templates/pages/frontblock1.tpl.php
+++ templates/pages/frontblock1.tpl.php
@@ -15,9 +15,9 @@ if($language_content->language == "es"){
 <div class="row">		
 		<div class="large-6 small-12 columns">
 			<div class="">
-				<h2 class="front-title"><span class="explora"><?php print t("Explore") ?></span> <span class="guara"><span>Honduras</span></span></h2>
+				<h2 class="front-title"><span class="guara"><?php print t("Explore") ?> <span>Honduras</span> </span></h2>
 				<div>
-					<h5 class="guara"></h5>
+					<h5 class="portal"><?php print t("the heart of Central America") ?></h5>
 				</div>
 				
 					<div class="img-h h"><a title="<?php print t("Hotels") ?>" href="<?php print $hotels ?>">Hotel</a></div>
