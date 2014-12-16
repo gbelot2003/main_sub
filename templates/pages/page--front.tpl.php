5bc3f89245d54321cbf374b092e15b37a7c03043
gbelot2003 <gbelot2003@hotmail.com>
2014-12-16 11:24:02 -0600

    adding structure to front pagas news & removing loader image

--- templates/pages/page--front.tpl.php
+++ templates/pages/page--front.tpl.php
@@ -63,28 +63,26 @@
     </div>
     
     <div class="l-frontpage">
+      <div class="row">
+        <div class="large-12 columns">
+          <!-- nota principal-->  
+          <?php print views_embed_view('views_front', $display_id = 'block_2');  ?>
+        </div>
+      </div>
 
       <div class="row">
-          <div class="small-12 columns">
-            <div class="large-12 columns">
-              <!-- nota principal-->  
-              <?php print views_embed_view('views_front', $display_id = 'block_2');  ?>
-            </div>
-          </div>
-
-          <div class="large-6 small-12 columns">
-            <div class="row">
-              <?php print views_embed_view('views_front', $display_id = 'block_11') ?>
-              
-              <div class="large-12 columns">
-                <?php print views_embed_view('views_front', $display_id = 'block_1') ?>
-              </div>
-              
-            <div class="large-5 columns">
-            
-            </div>
-          </div><!-- end row -->
+        <div class="large-7 columns">
+            <h3>Noticias</h3>
+            <?php print views_embed_view('views_front', $display_id = 'block_3');  ?>
         </div>  
+
+        <div class="large-5 small-12 columns">
+          <div class="row">
+            <?php print views_embed_view('views_front', $display_id = 'block_11') ?> 
+              <hr />
+            <?php print views_embed_view('views_front', $display_id = 'block_1') ?>         
+          </div><!-- end row -->
+        </div>
       </div>
 
       <hr style="visibility: hidden" /> 
@@ -116,7 +114,7 @@
 
                 <div class="large-12 columns blog-slide">
                   <!-- blog-->  
-                  <?php print views_embed_view('views_front', $display_id = 'block_3');  ?></div>
+                  </div>
               <div class="large-12 columns">
                 <h5><?php print t("Event's Calendar") ?></h5>
                 <?php print views_embed_view('eventos_y_festividades', $display_id = 'block');?></div>
