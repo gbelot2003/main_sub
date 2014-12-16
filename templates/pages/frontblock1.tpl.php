928afce33669ac8eecdf465c3c2bfe70adb766cc
gbelot2003 <gbelot2003@hotmail.com>
2014-12-16 03:25:47 -0600

    re-arrange page--front and delete frontblock-1 & 2

--- templates/pages/frontblock1.tpl.php
+++ templates/pages/frontblock1.tpl.php
@@ -1,17 +1,60 @@
-<div class="row">		
+<div class="row">
+		<div class="small-12 columns">
+			<div class="large-12 columns">
+				<!-- nota principal-->	
+				<?php print views_embed_view('views_front', $display_id = 'block_2');  ?>
+			</div>
+		</div>
+
 		<div class="large-6 small-12 columns">
 			<div class="row">
-
 				<?php print views_embed_view('views_front', $display_id = 'block_11') ?>
 				
 				<div class="large-12 columns">
 					<?php print views_embed_view('views_front', $display_id = 'block_1') ?>
 				</div>
 				
-				<div class="large-5 columns">
+			<div class="large-5 columns">
+			
 			</div>
 		</div><!-- end row -->
 	</div>	
 </div>
 
+<hr style="visibility: hidden" /> 
 
+<div class="row">
+	<div id="font-block-3" class="large-12 columns">
+		<hr style="visibility: hidden" />   
+		<div class="row">
+			<div class="large-5 small-12 columns">
+				<div class="large-12 columns video">
+					<?php print views_embed_view('views_front', $display_id = 'block_9');  ?></div>
+
+			<div class="large-12 columns">
+				<div class="large-12 columns">
+					<?php print views_embed_view('views_front', $display_id = 'block_6');  ?></div>
+				</div>
+				
+				<div class="large-12 columns">
+					<div class="hash">
+						<div class="row">
+							<div class="large-12 columns">
+								<?php print render($page['hash']) ?></div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="large-7 small-12 columns">
+
+					<div class="large-12 columns blog-slide">
+						<!-- blog-->	
+						<?php print views_embed_view('views_front', $display_id = 'block_3');  ?></div>
+				<div class="large-12 columns">
+					<h5><?php print t("Event's Calendar") ?></h5>
+					<?php print views_embed_view('eventos_y_festividades', $display_id = 'block');?></div>
+			</div>
+		</div>
+	</div>
+</div>
--- templates/pages/frontblock2.tpl.php
+++ templates/pages/frontblock2.tpl.php
@@ -1,26 +1,25 @@
 <div class="row">
+	<div class="large-5 small-12 columns">
+		<div class="large-12 columns video">
+			<?php print views_embed_view('views_front', $display_id = 'block_9');  ?></div>
 
-		<div class="large-5 small-12 columns">
-			<div class="large-12 columns video">
-				<?php print views_embed_view('views_front', $display_id = 'block_9');  ?></div>
-
-			<div class="large-12 columns">
-				<?php print views_embed_view('views_front', $display_id = 'block_6');  ?></div>
-
-			<div class="large-12 columns">
-				<div class="hash">
-					<div class="row">
-						<div class="large-12 columns">
-							<?php print render($page['hash']) ?></div>
+	<div class="large-12 columns">
+		<div class="large-12 columns">
+			<?php print views_embed_view('views_front', $display_id = 'block_6');  ?></div>
+		</div>
+		
+		<div class="large-12 columns">
+			<div class="hash">
+				<div class="row">
+					<div class="large-12 columns">
+						<?php print render($page['hash']) ?></div>
 					</div>
 				</div>
 			</div>
 		</div>
 
 		<div class="large-7 small-12 columns">
-			<div class="large-12 columns">
-				<!-- nota principal-->	
-				<?php print views_embed_view('views_front', $display_id = 'block_2');  ?></div>
+
 			<div class="large-12 columns blog-slide">
 				<!-- blog-->	
 				<?php print views_embed_view('views_front', $display_id = 'block_3');  ?></div>
